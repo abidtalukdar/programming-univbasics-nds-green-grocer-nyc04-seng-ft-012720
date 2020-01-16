814ce1d023431da8ca8fb5012cb4ace0bb96129d
Abid Talukdar <abiditalukdar@gmail.com>
2020-01-16 08:33:21 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -80,6 +80,7 @@ def checkout(cart, coupons)
   # * apply_coupons
   # * apply_clearance
   #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+  consolidated_cart= 
+  
+  
 end
